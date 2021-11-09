a51afd20dfee3fc59278a78e4d7df1f64afbca2f
vaisakhm99 <79189859+vaisakhm99@users.noreply.github.com>
2021-11-09 21:46:31 +0530

    Add files via upload

--- dat_to_str.py
+++ dat_to_str.py
@@ -4,8 +4,12 @@ Created on Mon Sep 20 11:09:06 2021
 
 @author: VAISAKH M
 """
-file=quantis
-outfile = open(file+".txt", "w")
-for n in open(file+"quantis1.dat", "rb").read():
+#converts a .dat file to .txt file
+
+file="quantis"
+
+outfile = open(file+".txt", "w")#opens output .txt file
+
+for n in open(file+".dat", "rb").read():#opens input .dat file
     outfile.write(f"{n:08b}")
 outfile.close()
--- partition_dat.py
+++ partition_dat.py
@@ -4,13 +4,18 @@ Created on Mon Sep 20 12:40:23 2021
 
 @author: VAISAKH M
 """
+#partitions a .dat file to multiple .dat files of equal size
+
 file='quantis'
-CHUNK_SIZE = 5000000
+
+CHUNK_SIZE = 5000000 #size of each file
+
 file_number = 1
-with open(file+'.dat','rb') as f:
+
+with open(file+'.dat','rb') as f:#opens main file
     chunk = f.read(CHUNK_SIZE)
     while chunk:
-        with open(file+ str(file_number)+'.dat','wb') as chunk_file:
+        with open(file+ str(file_number)+'.dat','wb') as chunk_file: #opens the output files
             chunk_file.write(chunk)
         file_number += 1
         chunk = f.read(CHUNK_SIZE)
